3b1c3b99b4a33adb50b651448201cad44b7505a2
Thayla Cristani <thaylacristanidev@gmail.com>
2024-04-22 14:21:03 -0300

    Iniciando Projeto

--- obj/Debug/net8.0/estrutura condicional 2.1.AssemblyInfo.cs
+++ obj/Debug/net8.0/estrutura condicional 2.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("estrutura condicional 2.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98bf817884cbd41496e6e26f112109b5992dd3de")]
 [assembly: System.Reflection.AssemblyProductAttribute("estrutura condicional 2.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("estrutura condicional 2.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
